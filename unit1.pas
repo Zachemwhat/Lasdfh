a40febd134ebcc8f5a1df2352cb9cbc522bb5b17
Zachemwhat <zachem.mda@gmail.com>
2017-03-01 13:53:16 +0300

    1c

---
+++ unit1.pas
@@ -0,0 +1,34 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, ExtCtrls,
+  StdCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Label1: TLabel;
+    Shape1: TShape;
+    Shape2: TShape;
+    Shape3: TShape;
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
